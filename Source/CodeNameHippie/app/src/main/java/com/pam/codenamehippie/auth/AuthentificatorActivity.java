3009aac6ed4aa9ec375b3c7d123442c8798dbe2e
carl <carlos8191970@gmail.com>
2015-11-17 14:09:50 -0500

    Ajouter classe et DOC

---
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/auth/AuthentificatorActivity.java
@@ -0,0 +1,16 @@
+package com.pam.codenamehippie.auth;
+
+import android.accounts.AccountAuthenticatorActivity;
+import android.os.Bundle;
+
+/**
+ * This class is only use for authentication and contains no view
+ * @see android.accounts.AccountAuthenticatorActivity
+ */
+public class AuthentificatorActivity extends AccountAuthenticatorActivity {
+
+    @Override
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+    }
+}
---
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/auth/HippieAuthenticator.java
@@ -0,0 +1,58 @@
+package com.pam.codenamehippie.auth;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * This class is used to manage authentication tokens and pass the requests to the server for authentication.
+ * @see android.accounts.AbstractAccountAuthenticator
+ * @see android.accounts.AccountManager
+ */
+public class HippieAuthenticator extends AbstractAccountAuthenticator {
+
+    private final Context context;
+
+    public HippieAuthenticator(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+        return null;
+    }
+
+    @Override
+    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public String getAuthTokenLabel(String authTokenType) {
+        return null;
+    }
+
+    @Override
+    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+        return null;
+    }
+
+    @Override
+    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
+        return null;
+    }
+}
