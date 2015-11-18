c77cac43dfd436c9d6bf3546c64ae7b5757e8014
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2015-11-18 17:05:28 -0500

    début d'une restructuration des layout et des themes. Correction d'une typo

--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/ConnexionActivity.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/ConnexionActivity.java
@@ -3,21 +3,20 @@ package com.pam.codenamehippie.ui;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
@@ -41,23 +40,91 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class ConnexionActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
+    private interface ProfileQuery {
+
+        String[] PROJECTION = {
+            ContactsContract.CommonDataKinds.Email.ADDRESS,
+            ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
+        };
+
+        int ADDRESS = 0;
+        int IS_PRIMARY = 1;
+    }
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mPassword;
+
+        UserLoginTask(String email, String password) {
+            this.mEmail = email;
+            this.mPassword = password;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            // TODO: attempt authentication against a network service.
+
+            try {
+                // Simulate network access.
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                return false;
+            }
+
+            for (String credential : DUMMY_CREDENTIALS) {
+                String[] pieces = credential.split(":");
+                if (pieces[0].equals(this.mEmail)) {
+                    // Account exists, return true if the password matches.
+                    return pieces[1].equals(this.mPassword);
+                }
+            }
+
+            // TODO: register the new account here.
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            ConnexionActivity.this.mAuthTask = null;
+            ConnexionActivity.this.showProgress(false);
+
+            if (success) {
+                ConnexionActivity.this.finish();
+            } else {
+                ConnexionActivity.this.mPasswordView
+                    .setError(ConnexionActivity.this.getString(R.string.error_incorrect_password));
+                ConnexionActivity.this.mPasswordView
+                    .requestFocus();
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            ConnexionActivity.this.mAuthTask = null;
+            ConnexionActivity.this.showProgress(false);
+        }
+    }
+
     /**
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
-
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+        "foo@example.com:hello", "bar@example.com:world"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
-
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
@@ -67,62 +134,64 @@ public class ConnexionActivity extends AppCompatActivity implements LoaderCallba
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connexion);
-        setupActionBar();
+        this.setContentView(R.layout.activity_connexion);
+        this.setupActionBar();
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
+        this.mEmailView = (AutoCompleteTextView) this.findViewById(R.id.email);
+        this.populateAutoComplete();
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        this.mPasswordView = (EditText) this.findViewById(R.id.password);
+        this.mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+                    ConnexionActivity.this.attemptLogin();
                     return true;
                 }
                 return false;
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) this.findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+                ConnexionActivity.this.attemptLogin();
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+        this.mLoginFormView = this.findViewById(R.id.login_form);
+        this.mProgressView = this.findViewById(R.id.login_progress);
     }
 
     private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
+        if (!this.mayRequestContacts()) {
             return;
         }
-
-        getLoaderManager().initLoader(0, null, this);
+        this.getLoaderManager().initLoader(0, null, this);
     }
 
     private boolean mayRequestContacts() {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             return true;
         }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+        if (this.checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
             return true;
         }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+        if (this.shouldShowRequestPermissionRationale(READ_CONTACTS)) {
+            Snackbar.make(this.mEmailView,
+                          R.string.permission_rationale,
+                          Snackbar.LENGTH_INDEFINITE)
                     .setAction(android.R.string.ok, new View.OnClickListener() {
                         @Override
                         @TargetApi(Build.VERSION_CODES.M)
                         public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+                            ConnexionActivity.this.requestPermissions(new String[]{READ_CONTACTS},
+                                                                      REQUEST_READ_CONTACTS);
                         }
                     });
         } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+            this.requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
         }
         return false;
     }
@@ -135,7 +204,7 @@ public class ConnexionActivity extends AppCompatActivity implements LoaderCallba
                                            @NonNull int[] grantResults) {
         if (requestCode == REQUEST_READ_CONTACTS) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
+                this.populateAutoComplete();
             }
         }
     }
@@ -147,7 +216,10 @@ public class ConnexionActivity extends AppCompatActivity implements LoaderCallba
     private void setupActionBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             // Show the Up button in the action bar.
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            ActionBar actionBar = this.getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+            }
         }
     }
 
@@ -157,36 +229,36 @@ public class ConnexionActivity extends AppCompatActivity implements LoaderCallba
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
+        if (this.mAuthTask != null) {
             return;
         }
 
         // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
+        this.mEmailView.setError(null);
+        this.mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
+        String email = this.mEmailView.getText().toString();
+        String password = this.mPasswordView.getText().toString();
 
         boolean cancel = false;
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
+        if (!TextUtils.isEmpty(password) && !this.isPasswordValid(password)) {
+            this.mPasswordView.setError(this.getString(R.string.error_invalid_password));
+            focusView = this.mPasswordView;
             cancel = true;
         }
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
+            this.mEmailView.setError(this.getString(R.string.error_field_required));
+            focusView = this.mEmailView;
             cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
+        } else if (!this.isEmailValid(email)) {
+            this.mEmailView.setError(this.getString(R.string.error_invalid_email));
+            focusView = this.mEmailView;
             cancel = true;
         }
 
@@ -197,9 +269,9 @@ public class ConnexionActivity extends AppCompatActivity implements LoaderCallba
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+            this.showProgress(true);
+            this.mAuthTask = new UserLoginTask(email, password);
+            this.mAuthTask.execute((Void) null);
         }
     }
 
@@ -222,48 +294,58 @@ public class ConnexionActivity extends AppCompatActivity implements LoaderCallba
         // for very easy animations. If available, use these APIs to fade-in
         // the progress spinner.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+            int shortAnimTime =
+                this.getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+            this.mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            this.mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                    ConnexionActivity.this.mLoginFormView.setVisibility(show
+                                                                        ? View.GONE
+                                                                        : View.VISIBLE);
                 }
             });
 
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            this.mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            this.mProgressView.animate().setDuration(shortAnimTime).alpha(
+                show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                    ConnexionActivity.this.mProgressView.setVisibility(show
+                                                                       ? View.VISIBLE
+                                                                       : View.GONE);
                 }
             });
         } else {
             // The ViewPropertyAnimator APIs are not available, so simply show
             // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            this.mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            this.mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
+                                // Retrieve data rows for the device user's 'profile' contact.
+                                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
+                                                     ContactsContract.Contacts.Data
+                                                         .CONTENT_DIRECTORY),
+                                ProfileQuery.PROJECTION,
 
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
+                                // Select only email addresses.
+                                ContactsContract.Contacts.Data.MIMETYPE +
+                                " = ?",
+                                new String[]{
+                                    ContactsContract.CommonDataKinds.Email
+                                        .CONTENT_ITEM_TYPE
+                                },
 
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
+                                // Show primary email addresses first. Note that there won't be
+                                // a primary email address if the user hasn't specified one.
+                                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
     }
 
     @Override
@@ -275,7 +357,7 @@ public class ConnexionActivity extends AppCompatActivity implements LoaderCallba
             cursor.moveToNext();
         }
 
-        addEmailsToAutoComplete(emails);
+        this.addEmailsToAutoComplete(emails);
     }
 
     @Override
@@ -283,81 +365,13 @@ public class ConnexionActivity extends AppCompatActivity implements LoaderCallba
 
     }
 
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
-
     private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
         //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
         ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(ConnexionActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
+            new ArrayAdapter<>(ConnexionActivity.this,
+                               android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
-        mEmailView.setAdapter(adapter);
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
+        this.mEmailView.setAdapter(adapter);
     }
 }
 
