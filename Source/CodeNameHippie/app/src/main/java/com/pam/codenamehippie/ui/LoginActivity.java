6636c2a5f719e9f90b71b3682176751ddb6a989f
ycourteau <ycourteau@collegeshawinigan.qc.ca>
2015-12-10 13:58:01 -0500

    Revert "Revert "Register""

--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/LoginActivity.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/LoginActivity.java
@@ -7,17 +7,20 @@ import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.util.Patterns;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.pam.codenamehippie.HippieApplication;
 import com.pam.codenamehippie.R;
+import com.pam.codenamehippie.controleur.validation.Validateur;
+import com.pam.codenamehippie.controleur.validation.ValidateurCourriel;
+import com.pam.codenamehippie.controleur.validation.ValidateurMotDePasse;
+import com.pam.codenamehippie.controleur.validation.ValidateurObserver;
+import com.pam.codenamehippie.http.Authentificateur;
 import com.pam.codenamehippie.modele.UtilisateurModele;
 import com.pam.codenamehippie.modele.UtilisateurModeleDepot;
 import com.squareup.okhttp.Callback;
@@ -29,226 +32,199 @@ import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements EditText.OnEditorActionListener,
+                                                                ValidateurObserver {
 
     private static final String TAG = LoginActivity.class.getSimpleName();
-    private EditText courrielEditText;
-    private EditText passwordEditText;
+    private OkHttpClient httpClient;
+    private ValidateurMotDePasse validateurMotDePasse;
+    private boolean motDePassEstValide;
+    private ValidateurCourriel validateurCourriel;
+    private boolean courrielEstValide;
     private Button loginButton;
     private SharedPreferences sharedPreferences;
-    private final TextWatcher formulaireTextWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            LoginActivity.this.validerFormulaire();
-        }
-    };
+    private Authentificateur authentificateur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.httpClient = ((HippieApplication) this.getApplication()).getHttpClient();
+        this.authentificateur = ((Authentificateur) this.httpClient.getAuthenticator());
         this.setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
         if (toolbar != null) {
             this.setSupportActionBar(toolbar);
         }
         this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        this.courrielEditText = ((EditText) this.findViewById(R.id.etCourriel));
+        this.validateurCourriel =
+                ValidateurCourriel.newInstance(this,
+                                               ((EditText) this.findViewById(R.id.etCourriel)),
+                                               true
+                                              );
         String rememberedEmail =
                 this.sharedPreferences.getString(this.getString(R.string.pref_email_key), null);
-        if ((rememberedEmail != null) && (this.courrielEditText != null)) {
-            this.courrielEditText.setText(rememberedEmail);
+        if ((rememberedEmail != null)) {
+            this.validateurCourriel.setText(rememberedEmail);
         }
-        String rememberedPassword =
-                this.sharedPreferences.getString(this.getString(R.string.pref_password_key), null);
-        this.passwordEditText = (EditText) this.findViewById(R.id.etPassword);
-        if ((this.passwordEditText != null) && (rememberedPassword != null)) {
-            this.passwordEditText.setText(rememberedPassword);
-        }
-        this.loginButton = (Button) this.findViewById(R.id.bLogin);
+        this.validateurMotDePasse =
+                ValidateurMotDePasse.newInstance(this,
+                                                 ((EditText) this.findViewById(R.id.etPassword))
+                                                );
+        this.validateurMotDePasse.getEditText().setOnEditorActionListener(this);
+        this.loginButton = ((Button) this.findViewById(R.id.bLogin));
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        if (this.courrielEditText != null) {
-            this.courrielEditText.removeTextChangedListener(this.formulaireTextWatcher);
-        }
-        if (this.passwordEditText != null) {
-            this.passwordEditText.removeTextChangedListener(this.formulaireTextWatcher);
-        }
+    protected void onPause() {
+        super.onPause();
+        this.validateurCourriel.onPause();
+        this.validateurMotDePasse.onPause();
+        this.validateurCourriel.unregisterObserver(this);
+        this.validateurMotDePasse.unregisterObserver(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        this.validerFormulaire();
+        this.validateurCourriel.onResume();
+        this.validateurMotDePasse.onResume();
+        this.validateurCourriel.registerObserver(this);
+        this.validateurMotDePasse.registerObserver(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (this.courrielEditText != null) {
-            this.courrielEditText.addTextChangedListener(this.formulaireTextWatcher);
-        }
-        if (this.passwordEditText != null) {
-            this.passwordEditText.addTextChangedListener(this.formulaireTextWatcher);
+        if (this.authentificateur.estAuthentifie()) {
+            this.navigueAMainActivity();
         }
     }
 
-    /**
-     * Methode pour vérifier si le champ courriel du formulaire est valide et update la vue en
-     * conséquence.
-     *
-     * @return True si le champ est valide
-     */
-    private boolean courrielEstIlValide() {
-        if ((this.courrielEditText != null)) {
-            Editable text = this.courrielEditText.getText();
-            String errorMessage = null;
-            if (TextUtils.isEmpty(text)) {
-                errorMessage = this.getString(R.string.error_field_required);
-            } else if (!Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
-                errorMessage = this.getString(R.string.error_invalid_email);
-            }
-            this.courrielEditText.setError(errorMessage);
-            if (errorMessage == null) {
-                this.sharedPreferences.edit()
-                                      .putString(this.getString(R.string.pref_email_key),
-                                                 text.toString()
-                                                )
-                                      .commit();
-            }
-            return this.courrielEditText.getError() == null;
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        boolean handled = false;
+        switch (actionId) {
+            case EditorInfo.IME_ACTION_DONE:
+                if (this.loginButton.isEnabled()) {
+                    this.onClickLogin(v);
+                }
+                handled = true;
+                break;
+            default:
+                break;
         }
-        return false;
+        return handled;
     }
 
-
-    /**
-     * Methode pour vérifier si le champ mot the passse du formulaire est valide et update la vue
-     * en conséquence.
-     *
-     * @return True si le champ est valide
-     */
-    private boolean motDePasseEstIlValide() {
-        if ((this.passwordEditText != null)) {
-            Editable text = this.passwordEditText.getText();
-            String errorMessage = null;
-            if (TextUtils.isEmpty(text)) {
-                errorMessage = this.getString(R.string.error_field_required);
-            }
-            // TODO: Checker les contraintes de mots de passe.
-            if (errorMessage == null) {
-                this.sharedPreferences.edit()
-                                      .putString(this.getString(R.string.pref_password_key),
-                                                 text.toString()
-                                                )
-                                      .commit();
-            }
-            this.passwordEditText.setError(errorMessage);
-            return this.passwordEditText.getError() == null;
+    @Override
+    public void enValidatant(Validateur validateur, boolean estValide) {
+        if (validateur.equals(this.validateurCourriel)) {
+            this.courrielEstValide = estValide;
+        } else if (validateur.equals(this.validateurMotDePasse)) {
+            this.motDePassEstValide = estValide;
         }
-        return false;
+        this.loginButton.setEnabled(this.motDePassEstValide && this.courrielEstValide);
     }
 
     /**
      * Methode pour ouvrir le formulaire d'inscription d'un  nouvel utilisateur
      */
-    public void onClickInscription(final View v) {
+    public void onClickInscription(View v) {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         LoginActivity.this.startActivity(intent);
-        //LoginActivity.this.finish();
-        // TODO: ajouter le parent dans le manifest pour le retour au parent
     }
 
     /**
-     * Methode pour vérifier si les champs mot the passse et courriel du formulaire est valide sont
-     * update la vue en conséquence.
+     * Methode déclechant la connection.
+     *
+     * @param v
+     *         un objet view qui est en lien avec l'interaction de connection.
      */
-    private void validerFormulaire() {
-        this.loginButton.setEnabled(LoginActivity.this.courrielEstIlValide() &&
-                                    LoginActivity.this.motDePasseEstIlValide());
-    }
-
     public void onClickLogin(final View v) {
-        OkHttpClient client = ((HippieApplication) this.getApplication()).getHttpClient();
         RequestBody requestBody =
                 new FormEncodingBuilder().add("courriel",
-                                              this.courrielEditText.getText().toString()
+                                              this.validateurCourriel.getText().toString()
                                              )
                                          .add("mot_de_passe",
-                                              this.passwordEditText.getText().toString()
+                                              this.validateurMotDePasse.getText().toString()
                                              )
                                          .build();
         Request request =
-                new Request.Builder().url(HippieApplication.baseUrl)
-                                     .post(requestBody)
-                                     .build();
-        final String prefPasswordKey = this.getString(R.string.pref_password_key);
-        client.newCall(request).enqueue(new Callback() {
+                new Request.Builder().url(HippieApplication.baseUrl).post(requestBody).build();
+        this.httpClient.newCall(request).enqueue(new Callback() {
+
             @Override
             public void onFailure(Request request, IOException e) {
                 LoginActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Snackbar.make(v, "Échec de connexion", Snackbar.LENGTH_SHORT).show();
+                        Snackbar.make(v, R.string.error_connection, Snackbar.LENGTH_SHORT).show();
                     }
                 });
                 // On oublie le mot de passe. Parce qu'on a échoué.
-                LoginActivity.this.sharedPreferences.edit().remove(prefPasswordKey).commit();
+                Authentificateur authentificateur =
+                        ((Authentificateur) LoginActivity.this.httpClient.getAuthenticator());
+                authentificateur.setMotDePasse(null);
             }
 
             @Override
             public void onResponse(Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    HippieApplication application =
-                            ((HippieApplication) LoginActivity.this.getApplication());
-                    UtilisateurModeleDepot utilisateurModeleDepot =
-                            application.getUtilisateurModeleDepot();
-                    String json = response.body().string();
-                    UtilisateurModele modele = utilisateurModeleDepot.ajouterModele(json, false);
-                    if (modele != null) {
-                        Log.d(TAG, "utilisateur: " + modele);
-                        // On roule dans un thread en parallel au main thread. Android demande
-                        // que les interaction avec l'UI soit sur le main thread.
-                        LoginActivity.this.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                LoginActivity.this.startActivity(intent);
-                                LoginActivity.this.finish();
-                            }
-                        });
-                    } else {
-                        LoginActivity.this.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Snackbar.make(v, "Échec de connexion", Snackbar.LENGTH_SHORT)
-                                        .show();
-                            }
-                        });
-                    }
-                } else {
+                if (!response.isSuccessful()) {
+                    // TODO: Gérer Mauvais mot de passe/courriel comme du monde.
                     LoginActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            Snackbar.make(v, "Échec de connexion", Snackbar.LENGTH_SHORT).show();
+                            Snackbar.make(v, R.string.error_connection, Snackbar.LENGTH_SHORT)
+                                    .show();
                         }
                     });
-                    // On oublie le mot de passe. Parce qu'on a échoué.
-                    LoginActivity.this.sharedPreferences.edit().remove(prefPasswordKey).commit();
+                } else {
+                    HippieApplication application =
+                            ((HippieApplication) LoginActivity.this.getApplication());
+                    UtilisateurModeleDepot depotUtilisateur =
+                            application.getUtilisateurModeleDepot();
+                    String json = response.body().string();
+                    UtilisateurModele utilisateur = depotUtilisateur.fromJson(json);
+                    String nom = utilisateur.getPrenom() + " " + utilisateur.getNom();
+                    LoginActivity.this.sauvegarderFormulaire();
+                    Snackbar.make(v,
+                                  LoginActivity.this.getString(R.string.message_welcome, nom),
+                                  Snackbar.LENGTH_SHORT
+                                 )
+                            .setCallback(new Snackbar.Callback() {
+                                @Override
+                                public void onDismissed(Snackbar snackbar, int event) {
+                                    super.onDismissed(snackbar, event);
+                                    LoginActivity.this.navigueAMainActivity();
+                                }
+                            })
+                            .show();
                 }
             }
         });
     }
+
+    /**
+     * Methode d'aide pour démarrer sur le main thread la main activity et appeler finish.
+     */
+    private void navigueAMainActivity() {
+        LoginActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                LoginActivity.this.startActivity(intent);
+                LoginActivity.this.finish();
+            }
+        });
+    }
+
+    private void sauvegarderFormulaire() {
+        this.authentificateur.setMotDePasse(this.validateurCourriel.getText().toString());
+        this.sharedPreferences.edit()
+                              .putString(this.getString(R.string.pref_email_key),
+                                         this.validateurMotDePasse.getText().toString()
+                                        )
+                              .commit();
+    }
+
 }
