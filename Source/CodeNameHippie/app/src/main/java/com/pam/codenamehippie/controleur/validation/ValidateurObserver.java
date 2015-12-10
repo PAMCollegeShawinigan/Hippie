a646f9d89c379cb25d36472020ac8bcf31276cd5
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2015-12-10 00:42:38 -0500

    Register is kinda done, just needs testing

--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/controleur/validation/ValidateurObserver.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/controleur/validation/ValidateurObserver.java
@@ -1,7 +1,8 @@
 package com.pam.codenamehippie.controleur.validation;
 
 /**
- * Interface pour les objet qui veulent observer des Validateurs
+ * Interface pour les objet qui veulent observer des Validateurs. Sert, entre autre à remplir le
+ * parametre T de {@link android.database.Observable}
  */
 public interface ValidateurObserver {
 
--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/RegisterActivity.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/RegisterActivity.java
@@ -1,11 +1,20 @@
 package com.pam.codenamehippie.ui;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.pam.codenamehippie.HippieApplication;
 import com.pam.codenamehippie.R;
 import com.pam.codenamehippie.controleur.validation.Validateur;
 import com.pam.codenamehippie.controleur.validation.ValidateurCourriel;
@@ -13,9 +22,18 @@ import com.pam.codenamehippie.controleur.validation.ValidateurDeChampTexte;
 import com.pam.codenamehippie.controleur.validation.ValidateurMotDePasse;
 import com.pam.codenamehippie.controleur.validation.ValidateurObserver;
 import com.pam.codenamehippie.http.Authentificateur;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.HttpUrl;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
-public class RegisterActivity extends AppCompatActivity implements ValidateurObserver {
+import java.io.IOException;
+
+public class RegisterActivity extends AppCompatActivity
+        implements ValidateurObserver, EditText.OnEditorActionListener {
 
     private OkHttpClient httpClient;
     private ValidateurDeChampTexte validateurNom;
@@ -30,11 +48,15 @@ public class RegisterActivity extends AppCompatActivity implements ValidateurObs
     private boolean courrielEstValide;
     private Button loginButton;
     private Authentificateur authentificateur;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_register);
+        this.httpClient = ((HippieApplication) this.getApplication()).getHttpClient();
+        this.authentificateur = ((Authentificateur) this.httpClient.getAuthenticator());
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
         if (toolbar != null) {
             this.setSupportActionBar(toolbar);
@@ -125,5 +147,90 @@ public class RegisterActivity extends AppCompatActivity implements ValidateurObs
                                     this.prenomEstValide &&
                                     this.nomEstValide &&
                                     this.courrielEstValide);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        boolean handled = false;
+        switch (actionId) {
+            case EditorInfo.IME_ACTION_DONE:
+                if (this.loginButton.isEnabled()) {
+                    this.soummettreLaConnexion(v);
+                }
+                handled = true;
+                break;
+            default:
+                break;
+        }
+        return handled;
+    }
+
+    /**
+     * Methode déclechant la connection.
+     *
+     * @param v
+     *         un objet view qui est en lien avec l'interaction de connection.
+     */
+    public void soummettreLaConnexion(final View v) {
+        RequestBody body =
+                new FormEncodingBuilder().add("nom", this.validateurNom.getTextString())
+                                         .add("prenom", this.validateurPrenom.getTextString())
+                                         .add("mot_de_passe",
+                                              this.validateurMotDePasse.getTextString()
+                                             )
+                                         .add("courriel", this.validateurCourriel.getTextString())
+                                         .build();
+        HttpUrl url = HippieApplication.baseUrl.newBuilder().addPathSegment("register").build();
+        Request request = new Request.Builder().url(url).post(body).build();
+        this.httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                RegisterActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Snackbar.make(v, R.string.error_connection, Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+                // On oublie le mot de passe. Parce qu'on a échoué.
+                Authentificateur authentificateur =
+                        ((Authentificateur) RegisterActivity.this.httpClient.getAuthenticator());
+                authentificateur.setMotDePasse(null);
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if (!response.isSuccessful()) {
+
+                } else {
+                    RegisterActivity.this.sauvegarderFormulaire();
+                    RegisterActivity.this.navigueAMainActivity();
+                }
+            }
+        });
+
+    }
+
+    private void sauvegarderFormulaire() {
+        this.authentificateur.setMotDePasse(this.validateurCourriel.getText().toString());
+        this.sharedPreferences.edit()
+                              .putString(this.getString(R.string.pref_email_key),
+                                         this.validateurMotDePasse.getText().toString()
+                                        )
+                              .commit();
+    }
+
+    /**
+     * Methode d'aide pour démarrer sur le main thread la main activity et appeler finish.
+     */
+    private void navigueAMainActivity() {
+        RegisterActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                RegisterActivity.this.startActivity(intent);
+                RegisterActivity.this.finish();
+            }
+        });
     }
 }
