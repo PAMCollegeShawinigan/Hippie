597711f4a8f15a8eab586b98064269d551630bed
Faust-36 <ericjolymusic@gmail.com>
2015-12-15 09:34:09 -0500

    modification

--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/MenuActivity.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/MenuActivity.java
@@ -1,13 +1,14 @@
 package com.pam.codenamehippie.ui;
 
+<<<<<<< Updated upstream
 import android.app.Fragment;
 import android.content.Context;
+=======
+import android.app.Activity;
+>>>>>>> Stashed changes
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.TextView;
@@ -21,7 +22,7 @@ import com.pam.codenamehippie.ui.view.trianglemenu.TriangleLayout.OnItemClickLis
 import com.pam.codenamehippie.ui.view.trianglemenu.TriangleLayout.OnItemSelectedListener;
 import com.pam.codenamehippie.ui.view.trianglemenu.TriangleLayout.OnRotationFinishedListener;
 
-public class MenuActivity extends Fragment implements OnItemSelectedListener,
+public class MenuActivity extends Activity implements OnItemSelectedListener,
         OnItemClickListener,
         OnRotationFinishedListener,
         OnCenterClickListener {
@@ -29,76 +30,52 @@ public class MenuActivity extends Fragment implements OnItemSelectedListener,
     private TextView selectedTextView;
     private TextView selectedTextView2;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public MenuActivity() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MenuActivity newInstance(String param1, String param2) {
-        MenuActivity fragment = new MenuActivity();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
   }
+=======
+>>>>>>> Stashed changes
 
-    @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container,
-                             Bundle savedInstanceState)
-    {
+        // Set content view by passed extra
+        // Bundle extras = getIntent().getExtras();
+        // int layoutId = extras.getInt(ARG_LAYOUT);
+        this.setContentView(R.layout.main_circle_layout);
 
         //intégrer les polices sur le menu
-        Typeface myTypeface = Typeface.createFromAsset(getActivity().getAssets(), "opensans_light.ttf");
-        TextView myTextview = (TextView)getView().findViewById(R.id.main_selected_textView);
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "opensans_light.ttf");
+        TextView myTextview = (TextView)findViewById(R.id.main_selected_textView);
         myTextview.setTypeface(myTypeface);
 
         // Set listeners
+<<<<<<< Updated upstream
         TriangleLayout triangleMenu = (TriangleLayout) getView().findViewById(R.id.main_menu_triangle);
+=======
+        TriangleLayout triangleMenu = (TriangleLayout) findViewById(R.id.main_circle_layout);
+>>>>>>> Stashed changes
         triangleMenu.setOnItemSelectedListener(this);
         triangleMenu.setOnItemClickListener(this);
         triangleMenu.setOnRotationFinishedListener(this);
         triangleMenu.setOnCenterClickListener(this);
 
-        selectedTextView = (TextView) getView().findViewById(R.id.main_selected_textView);
+        selectedTextView = (TextView) findViewById(R.id.main_selected_textView);
         selectedTextView.setText(((TriangleImageView) triangleMenu
                 .getSelectedItem()).getName());
 
+<<<<<<< Updated upstream
         selectedTextView2 = (TextView) getView().findViewById(R.id.main_selected_textView2);
         selectedTextView2.setText(((TriangleImageView) triangleMenu
                 .getSelectedItem()).getName());
 
         setHasOptionsMenu(true);
         return inflater.inflate(R.layout.main_menu, container, false);
+=======
+>>>>>>> Stashed changes
     }
 
     @Override
@@ -130,8 +107,13 @@ public class MenuActivity extends Fragment implements OnItemSelectedListener,
     @Override
     public void onItemClick(View view, String name) {
 
+<<<<<<< Updated upstream
         Toast.makeText(this.getActivity().getApplicationContext(),
                 " Nouvelle activité ",
+=======
+        Toast.makeText(getApplicationContext(),
+                " Nouvelle activitée ",
+>>>>>>> Stashed changes
                 Toast.LENGTH_SHORT).show();
 
         switch (view.getId()) {
@@ -166,48 +148,12 @@ public class MenuActivity extends Fragment implements OnItemSelectedListener,
 
     @Override
     public void onCenterClick() {
+<<<<<<< Updated upstream
         Toast.makeText(this.getActivity().getApplicationContext(), "LE CENTRE A ÉTÉ CLICKÉ",
+=======
+        Toast.makeText(getApplicationContext(), "activitée du centre",
+>>>>>>> Stashed changes
                 Toast.LENGTH_SHORT).show();
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 
 
