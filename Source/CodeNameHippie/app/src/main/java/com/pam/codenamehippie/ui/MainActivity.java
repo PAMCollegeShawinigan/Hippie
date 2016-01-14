c7fc0402f3f63c6844efcd9a1a133170baba9bdd
Faust-36 <ericjolymusic@gmail.com>
2016-01-14 08:51:08 -0500

    changement de résolution logo2

--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/MainActivity.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/MainActivity.java
@@ -1,10 +1,14 @@
 package com.pam.codenamehippie.ui;
 
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.pam.codenamehippie.HippieApplication;
 import com.pam.codenamehippie.R;
 import com.pam.codenamehippie.modele.AlimentaireModeleDepot;
@@ -12,17 +16,25 @@ import com.pam.codenamehippie.modele.UtilisateurModeleDepot;
 
 public class MainActivity extends HippieActivity {
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
-        
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "opensans_regular.ttf");
+
+        Typeface typeface;
+        typeface = Typeface.createFromAsset(getAssets(), "opensans_regular.ttf");
         ((TextView) findViewById(R.id.textView5)).setTypeface(typeface);
         ((TextView) findViewById(R.id.textView10)).setTypeface(typeface);
         ((TextView) findViewById(R.id.textView17)).setTypeface(typeface);
 
-        Typeface typeface2 = Typeface.createFromAsset(getAssets(), "opensans_light.ttf");
+        Typeface typeface2;
+        typeface2 = Typeface.createFromAsset(getAssets(), "opensans_light.ttf");
         ((TextView) findViewById(R.id.textView7)).setTypeface(typeface2);
         ((TextView) findViewById(R.id.textView11)).setTypeface(typeface2);
         ((TextView) findViewById(R.id.textView13)).setTypeface(typeface2);
@@ -35,6 +47,49 @@ public class MainActivity extends HippieActivity {
                         .getUtilisateurModeleDepot();
         alimentaireModeleDepot.peuplerLesListes();
         utilisateurModeleDepot.peuplerLeDepot();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.pam.codenamehippie.ui/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.pam.codenamehippie.ui/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
 
