b4146f1000e8b97ebde323f46eb4c591487935fd
PALachance <pieralainlachance@gmail.com>
2016-01-18 14:57:52 -0500

    ajout fonction listeDonneur

--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/MainActivity.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import com.pam.codenamehippie.HippieApplication;
 import com.pam.codenamehippie.R;
 import com.pam.codenamehippie.modele.AlimentaireModeleDepot;
+import com.pam.codenamehippie.modele.OrganismeModeleDepot;
 import com.pam.codenamehippie.modele.UtilisateurModeleDepot;
 
 public class MainActivity extends HippieActivity {
@@ -45,6 +46,9 @@ public class MainActivity extends HippieActivity {
         AlimentaireModeleDepot alimentaireModeleDepot =
                 ((HippieApplication) this.getApplication()).getAlimentaireModeleDepot();
         alimentaireModeleDepot.peuplerListeDon(4);
+        OrganismeModeleDepot organismeModeleDepot =
+                ((HippieApplication) this.getApplication()).getOrganismeModeleDepot();
+        organismeModeleDepot.peuplerListeDonneur();
     }
 }
 
