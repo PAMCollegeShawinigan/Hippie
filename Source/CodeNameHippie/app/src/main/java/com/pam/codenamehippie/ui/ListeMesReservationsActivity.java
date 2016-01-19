cceb856b606b4c5a9226314f377e9fc751db046a
Mysticluna <hime.neeko@gmail.com>
2016-01-19 12:08:07 -0500

    Modif progress bar + Ajout MesReservations

--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/ListeMesReservationsActivity.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/ListeMesReservationsActivity.java
@@ -1,6 +1,15 @@
 package com.pam.codenamehippie.ui;
 
+import android.os.Bundle;
 import android.widget.ListView;
+
+import com.pam.codenamehippie.HippieApplication;
+import com.pam.codenamehippie.R;
+import com.pam.codenamehippie.modele.AlimentaireModele;
+import com.pam.codenamehippie.modele.AlimentaireModeleDepot;
+import com.pam.codenamehippie.ui.adapter.MesReservationsAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Catherine on 2016-01-13.
@@ -11,6 +20,27 @@ public class ListeMesReservationsActivity extends HippieActivity {
     // Informations sur les listes et les listAdapter : http://www.vogella.com/tutorials/AndroidListView/article.html
 
     ListView mesReservations;
+    MesReservationsAdapter mesReservationsAdapter;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.liste_reservations);
+
+        AlimentaireModeleDepot alimentaireModeleDepot = ((HippieApplication) this.getApplication()).getAlimentaireModeleDepot();
+
+        // Filtre pour récupérer les items dont le statut est réservé par le receveur (AKA organisme)
+        ArrayList<AlimentaireModele> modeles = new ArrayList<>();
+
+        // TODO: Vérifier ce qui est disponible dans cette dite de ListeDon()
+        for (AlimentaireModele modele : alimentaireModeleDepot.getListeDon()) {
+            String statut = modele.getStatut();
+            int idReceveur = modele.getId();
+
+            if (statut.equalsIgnoreCase("Reservé"))
+
+        }
+
+    }
 
 }
--- Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/adapter/MesReservationsAdapter.java
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/adapter/MesReservationsAdapter.java
@@ -1,15 +1,21 @@
 package com.pam.codenamehippie.ui.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.pam.codenamehippie.R;
 import com.pam.codenamehippie.modele.AlimentaireModele;
 import com.pam.codenamehippie.modele.AlimentaireModeleDepot;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by Catherine on 2016-01-19.
@@ -53,14 +59,35 @@ public class MesReservationsAdapter extends BaseAdapter {
      */
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         View row = convertView;
 
         final AlimentaireModele modele = this.getItem(position);
         if (row == null) {
             LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.liste_reservations_row, parent, false);
         }
 
+        ((TextView) row.findViewById(R.id.tv_res_nom_marchandise)).setText(modele.getNom());
+        ((TextView) row.findViewById(R.id.tv_dons_description_marchandise)).setText(modele.getDescription());
+
+        // On affiche la quantité + l'unité de la marchandise
+        String quantiteString = modele.getQuantite().toString() + " " + modele.getUnite();
+        ((TextView) row.findViewById(R.id.tv_res_qtee_marchandise)).setText(quantiteString);
+        ((TextView) row.findViewById(R.id.tv_res_date_marchandise)).setText((CharSequence) modele.getDatePeremption());
+
+        // Supprimer une réservation de la liste
+        ImageButton ibSupprimer = (ImageButton) row.findViewById(R.id.ib_res_supprimer);
+        ibSupprimer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // TODO: faire ce qui faut pour supprimer une marchandise de la liste.
+                Log.i("Boutton supprimé cliqué", "**********" + position);
+
+                Toast.makeText(context, "Réservation supprimé de la liste",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
         return row;
     }
 }
