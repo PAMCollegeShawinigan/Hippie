850c4e25314cd56dcddf131f1d06a534f0c9742d
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2016-02-06 20:45:26 -0500

    Oups

--- Source/CodeNameHippie/app/src/test/java/com/pam/codenamehippie/modele/AdresseModeleTest.java
+++ Source/CodeNameHippie/app/src/test/java/com/pam/codenamehippie/modele/AdresseModeleTest.java
@@ -17,51 +17,56 @@ import static org.junit.Assert.assertTrue;
  */
 public class AdresseModeleTest {
 
-    private AdresseModele addresse1;
-    private AdresseModele address2;
+    private AdresseModele adresse1;
+    private AdresseModele adresse2;
 
     @Before
     public void setUp() {
-        this.addresse1 = new AdresseModele().setId(1)
-                                            .setTypeRue("Rue")
-                                            .setNom("Gouin")
-                                            .setCodePostal("G9N7P6")
-                                            .setVille("Shawinigan")
-                                            .setPays("Canada")
-                                            .setProvince("Quebec");
-        this.address2 = new AdresseModele().setId(2)
+        this.adresse1 = new AdresseModele().setId(1)
+                                           .setTypeRue("Rue")
+                                           .setNom("Gouin")
+                                           .setCodePostal("G9N7P6")
+                                           .setVille("Shawinigan")
+                                           .setPays("Canada")
+                                           .setProvince("Quebec");
+        this.adresse2 = new AdresseModele().setId(2)
                                            .setTypeRue("Avenue")
                                            .setNom("de la Station")
                                            .setCodePostal("g9n 8k9")
                                            .setVille("Shawinigan")
                                            .setPays("Canada")
                                            .setProvince("Quebec");
+        for (Field field : AdresseModele.class.getDeclaredFields()) {
+            field.setAccessible(true);
+        }
     }
 
     @After
     public void tearDown() {
-        this.addresse1 = null;
-        this.address2 = null;
+        this.adresse1 = null;
+        this.adresse2 = null;
+        for (Field field : AdresseModele.class.getDeclaredFields()) {
+            field.setAccessible(false);
+        }
     }
 
     @Test
     public void setCodePostalShouldStripSpacesAndUpperCase() {
         Assert.assertFalse("Code postal shouldn't contains spaces",
-                           this.address2.getCodePostal().contains(" ")
+                           this.adresse2.getCodePostal().contains(" ")
                           );
         Assert.assertFalse("Code postal should be uppercase",
-                           this.address2.getCodePostal().equals("g9n8k9")
+                           this.adresse2.getCodePostal().equals("g9n8k9")
                           );
     }
 
     @Test
     public void formattedCodePostalShouldBeLazilyInstantiated() throws Exception {
-        Field field = this.addresse1.getClass().getDeclaredField("formattedCodePostal");
-        field.setAccessible(true);
-        String value = ((String) field.get(this.addresse1));
+        Field field = this.adresse1.getClass().getDeclaredField("formattedCodePostal");
+        String value = ((String) field.get(this.adresse1));
         assertNull("formattedCodePostal should be null at start", value);
-        String formattedCodePostal = this.addresse1.getFormattedCodePostal();
-        value = ((String) field.get(this.addresse1));
+        String formattedCodePostal = this.adresse1.getFormattedCodePostal();
+        value = ((String) field.get(this.adresse1));
         assertNotNull("getFormattedCodePostal should cache its value",
                       value
                      );
@@ -72,27 +77,37 @@ public class AdresseModeleTest {
         assertTrue("formattedCodePostal should be formatted",
                    formattedCodePostal.contains(" ")
                   );
-        field.setAccessible(false);
     }
 
     @Test
     public void formattedStringShouldBeLazilyInstantiated() throws Exception {
-        Field field = this.addresse1.getClass().getDeclaredField("formattedString");
-        field.setAccessible(true);
-        String value = ((String) field.get(this.addresse1));
+        Field field = this.adresse1.getClass().getDeclaredField("formattedString");
+        String value = ((String) field.get(this.adresse1));
         assertNull("formattedString should be null at start", value);
-        String formattedString = this.addresse1.toFormattedString();
-        value = ((String) field.get(this.addresse1));
+        String formattedString = this.adresse1.toFormattedString();
+        value = ((String) field.get(this.adresse1));
         assertNotNull("toFormattedString should cache its value", value);
         assertEquals("toFormattedString should return cached value",
                      value,
                      formattedString
                     );
     }
+    
+    @Test
+    public void setCodePostalShouldInvalidateCachedStrings() throws Exception {
+        Field string = this.adresse1.getClass().getDeclaredField("formattedString");
+        Field code = this.adresse1.getClass().getDeclaredField("formattedCodePostal");
+        this.adresse1.toFormattedString();
+        assertNotNull("toFormattedString should fill string cache", string.get(this.adresse1));
+        assertNotNull("toFormattedString should fill string cache", code.get(this.adresse1));
+        this.adresse1.setCodePostal("H0H 0H0");
+        assertNull("setCodePostal should empty string cache", string.get(this.adresse1));
+        assertNull("setCodePostal should empty string cache", code.get(this.adresse1));
+
+    }
 
     @Test
-    @SuppressWarnings("EqualsWithItself")
-    public void equalsTests() {
+    public void equalityTests() {
         AdresseModele addr1 = new AdresseModele().setId(1)
                                                  .setTypeRue("Rue")
                                                  .setNom("Gouin")
@@ -100,10 +115,15 @@ public class AdresseModeleTest {
                                                  .setVille("Shawinigan")
                                                  .setPays("Canada")
                                                  .setProvince("Quebec");
-        assertEquals("Same variable should be equal", this.addresse1, this.addresse1);
+        assertEquals("Same variable should be equal", this.adresse1, this.adresse1);
+        assertEquals("Same values should be equal", this.adresse1, addr1);
+        assertEquals("Same values hashcode should be equal",
+                     this.adresse1.hashCode(),
+                     addr1.hashCode()
+                    );
         Assert.assertNotEquals("Different addresses shouldn't be equal",
-                               this.addresse1,
-                               this.address2
+                               this.adresse1,
+                               this.adresse2
                               );
     }
 }
