0145ba3cba268edc713eea878972cd9ab9da1f10
carl <carlos8191970@gmail.com>
2016-02-09 08:43:00 -0500

    Liste Statistiques (Layout)

---
+++ Source/CodeNameHippie/app/src/main/java/com/pam/codenamehippie/ui/ListeStatistiquesActivity.java
@@ -0,0 +1,53 @@
+package com.pam.codenamehippie.ui;
+
+import android.os.Bundle;
+
+import com.pam.codenamehippie.modele.AlimentaireModele;
+import com.pam.codenamehippie.modele.depot.ObservateurDeDepot;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Carl St-Louis le 2016-02-08.
+ */
+public class ListeStatistiquesActivity extends HippieActivity
+    implements ObservateurDeDepot<AlimentaireModele>{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void surDebutDeRequete() {
+
+    }
+
+    @Override
+    public void surChangementDeDonnees(ArrayList<AlimentaireModele> modeles) {
+
+    }
+
+    @Override
+    public void surFinDeRequete() {
+
+    }
+
+    @Override
+    public void surErreur(IOException e) {
+
+    }
+}
