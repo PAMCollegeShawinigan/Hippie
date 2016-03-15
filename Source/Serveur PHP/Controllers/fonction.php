f06ceecc07bcb283edbda19434d47e920d91e755
Philippe Lafontaine <lafontaine.philippe@gmail.com>
2016-03-15 17:54:09 -0400

    Fix la sérialisation de date au serveur

--- Source/Serveur PHP/Controllers/fonction.php
+++ Source/Serveur PHP/Controllers/fonction.php
@@ -3,43 +3,43 @@ function convertirdate($datebd) // retourne la date en format DATE_ATOM si la da
 {
 	if($datebd != null )
 	{
-				
+
 		$date = date_create($datebd);
-					
+
 		$date_peremption = date_format($date, DATE_ATOM);
-	
+
 	}
 	else
 	{
-		$date_peremption = 'null';
+		$date_peremption = null;
 	}
-			
+
 	return $date_peremption;
 }
-	
+
 function siperissable($resultat, $post) // si l'aliment est perissable retourne une date sinon retourne null
 {
 	$date_peremption = null;
-	
+
 	if($resultat == 0)
 	{
 		$dare_peremtion = $post;
 	}
 	return $date_peremption;
-}	
+}
 
 function sidisponible($id_alimentaire)// retourne vrai si l'aliment est toujours disponble sinon retourne faux
-{ 
+{
 	require('Connection/bdlogin.php');
-	
-	$req = 'SELECT marchandise_statut 
-								FROM alimentaire 
+
+	$req = 'SELECT marchandise_statut
+								FROM alimentaire
 								WHERE alimentaire_id = :alimentaire_id';
-								
+
 	$array = array('alimentaire_id' => $id_alimentaire);
-					
+
 		$resultat = execution($req, $array)->fetch();
-		
-	return($resultat['marchandise_status'] == 3);		
-		
+
+	return($resultat['marchandise_status'] == 3);
+
 }
